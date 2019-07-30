4132de991997bea0776c71557ee702080f90efba
mhfarrell <mhfarrell@users.noreply.github.com>
2019-07-30 16:34:37 +0100

    old code

---
+++ APIConnectionTest/DatabaseConnectionTest/DatabaseConnectionTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DatabaseConnectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnect.connectDB();
+            Console.WriteLine(DBConnect.serverVersion() + "\n\n");
+            DBConnect.closeDB();  
+
+            Console.WriteLine("Press Any Key To Exit");
+            Console.ReadKey();
+        }
+        }
+    }
